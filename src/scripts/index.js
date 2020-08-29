71517e885229dee2af2f97daf8e20a995c4c0b1b
ra-md <ra799m@gmail.com>
2020-08-30 06:12:33 +0700

    daftar restoran

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
+
 import '../styles/style.scss';
 
 import './nav';
+import './restoran';
---
+++ src/scripts/restoran.js
@@ -0,0 +1,29 @@
+import { restaurants } from '../DATA.json';
+
+const daftarRestortan = document.getElementById('daftar-restoran');
+
+function potongText(text, end) {
+  return `${text.split(' ').slice(0, end).join(' ')}...`;
+}
+
+restaurants.forEach(({
+  pictureId,
+  name,
+  city,
+  rating,
+  description,
+}) => {
+  daftarRestortan.innerHTML += `
+    <div class="restoran">
+      <img class="restoran__image" alt="restoran ${name}" src="${pictureId}"/>
+      <div class="restoran-body">
+        <h1 class="restoran-body__name">${name}</h1>
+        <p class="restoran-body__description">${potongText(description, 10)}</p>
+        <div class="city-rating">
+          <span class="city-rating__city"><i class="fas fa-map-marker-alt"></i>${city}</span>
+          <span class="city-rating__rating"><i class="fas fa-star"></i>${rating}</span>
+        </div>
+      </div>
+    </div>
+  `;
+});
