b320fe405b216a0b03f9d4780206532b371f8f0c
ra-md <ra799m@gmail.com>
2020-08-29 21:44:35 +0700

    fix body overflow

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,29 +1,17 @@
 const nav = document.querySelector('.nav');
 const headerMenu = document.querySelector('.header__menu');
-const jumbotron = document.querySelector('.jumbotron');
+const body = document.querySelector('body');
 
 headerMenu.addEventListener('click', (event) => {
   nav.classList.toggle('nav--open');
 
   if (headerMenu.innerHTML === '☰') {
     headerMenu.innerHTML = '✖';
+    body.style.overflowY = 'hidden';
   } else {
     headerMenu.innerHTML = '☰';
+    body.style.overflowY = 'auto';
   }
 
   event.stopPropagation();
-});
-
-function removeNav(event) {
-  nav.classList.remove('nav--open');
-  headerMenu.innerHTML = '☰';
-  event.stopPropagation();
-}
-
-jumbotron.addEventListener('click', (event) => {
-  removeNav(event);
-});
-
-jumbotron.addEventListener('click', (event) => {
-  removeNav(event);
 });
