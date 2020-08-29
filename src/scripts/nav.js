c25a36b5916a483e450020769c60531b5152062c
ra-md <ra799m@gmail.com>
2020-08-29 14:39:20 +0700

    add event listener to the main element

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('.nav');
 const headerMenu = document.querySelector('.header__menu');
+const main = document.querySelector('main');
 
 headerMenu.addEventListener('click', (event) => {
   nav.classList.toggle('nav--open');
@@ -10,5 +11,11 @@ headerMenu.addEventListener('click', (event) => {
     headerMenu.innerHTML = '☰';
   }
 
+  event.stopPropagation();
+});
+
+main.addEventListener('click', (event) => {
+  nav.classList.remove('nav--open');
+  headerMenu.innerHTML = '☰';
   event.stopPropagation();
 });
