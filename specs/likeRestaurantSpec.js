c01f50f0efa41510010e9c41edf3976509947a90
ra-md <ra799m@gmail.com>
2020-09-28 11:53:55 +0700

    rename to lower case

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -1,5 +1,5 @@
 import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurants';
-import * as TestFactories from './helpers/testFactories';
+import * as testFactories from './helpers/testFactories';
 
 describe('Liking a restaurant', () => {
   const addLikeButtonContainer = () => {
@@ -11,21 +11,21 @@ describe('Liking a restaurant', () => {
   });
 
   it('should show the like button when the restaurant has not been liked before', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
     expect(document.querySelector('[aria-label="Like this restaurant"]')).toBeTruthy();
   });
 
   it('should not show the unlike button when the restaurant has not been liked before', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
     expect(document.querySelector('[aria-label="Unlike this restaurant"]')).toBeFalsy();
   });
 
   it('should be able to like the restaurant', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
-    TestFactories.clickFavBtn();
+    testFactories.clickFavBtn();
 
     const restaurant = await FavoriteRestaurantIdb.get(1);
 
@@ -35,11 +35,11 @@ describe('Liking a restaurant', () => {
   });
 
   it('should not add a restaurant again when its already liked', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
 
     await FavoriteRestaurantIdb.put({ id: 1 });
 
-    TestFactories.clickFavBtn();
+    testFactories.clickFavBtn();
 
     expect(await FavoriteRestaurantIdb.getAll()).toEqual([{ id: 1 }]);
 
@@ -47,9 +47,9 @@ describe('Liking a restaurant', () => {
   });
 
   it('should not add a restaurant when it has no id', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({});
+    await testFactories.createLikeButtonPresenterWithRestaurant({});
 
-    TestFactories.clickFavBtn();
+    testFactories.clickFavBtn();
 
     expect(await FavoriteRestaurantIdb.getAll()).toEqual([]);
   });
--- specs/unlikeRestaurantSpec.js
+++ specs/unlikeRestaurantSpec.js
@@ -1,6 +1,5 @@
 import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurants';
-import * as TestFactories from './helpers/testFactories';
-
+import * as testFactories from './helpers/testFactories';
  
 describe('Unliking A Restaurant', () => {
   const addLikeButtonContainer = () => {
@@ -17,31 +16,31 @@ describe('Unliking A Restaurant', () => {
   });
  
   it('should display unlike widget when the restaurant has been liked', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
  
     expect(document.querySelector('[aria-label="Unlike this restaurant"]')).toBeTruthy();
   });
  
   it('should not display like widget when the restaurant has been liked', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
  
     expect(document.querySelector('[aria-label="Like this restaurant"]')).toBeFalsy();
   });
 
   it('should be able to remove liked restaurant from the list', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
    
-    TestFactories.clickFavBtn()
+    testFactories.clickFavBtn()
    
     expect(await FavoriteRestaurantIdb.getAll()).toEqual([]);
   });
 
   it('should not throw error if the unliked restaurant is not in the list', async () => {
-    await TestFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
+    await testFactories.createLikeButtonPresenterWithRestaurant({ id: 1 });
    
     await FavoriteRestaurantIdb.delete(1);
    
-    TestFactories.clickFavBtn()
+    testFactories.clickFavBtn()
    
     expect(await FavoriteRestaurantIdb.getAll()).toEqual([]);
   });
