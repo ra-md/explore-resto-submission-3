3000219b9a19c04ec3c8eb20579d23a3319443ea
ra-md <ra799m@gmail.com>
2020-09-28 11:52:54 +0700

    change arrow to regular function

--- specs/helpers/testFactories.js
+++ specs/helpers/testFactories.js
@@ -1,13 +1,13 @@
 import LikeButtonPresenter from '../../src/scripts/utils/like-button-presenter';
 import FavoriteRestaurantIdb from '../../src/scripts/data/favorite-restaurants';
 
-const createLikeButtonPresenterWithRestaurant = async (restaurant) => {
+async function createLikeButtonPresenterWithRestaurant(restaurant) {
   await LikeButtonPresenter.init({
     favButtonContainer: document.getElementById('favButtonContainer'),
     favoriteRestaurants: FavoriteRestaurantIdb,
     restaurant,
   });
-};
+}
 
 function clickFavBtn() {
   document.querySelector('#favButton').dispatchEvent(new Event('click'));
